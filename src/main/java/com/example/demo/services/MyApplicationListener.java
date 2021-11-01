9750c5fb8c2b4d04fb48442db272a85cff6eab5c
Andrei Sogomonian <saipeg@yandex.ru>
2021-11-01 13:41:56 +0300

    Added email sender.

--- src/main/java/com/example/demo/services/MyApplicationListener.java
+++ src/main/java/com/example/demo/services/MyApplicationListener.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
 
+import static com.example.demo.services.EmailSender.sendFromGMail;
+
 @Component
 public class MyApplicationListener implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
     private static final Logger LOG = Logger.getLogger(String.valueOf(MyApplicationListener.class));
@@ -16,5 +18,15 @@ public class MyApplicationListener implements ApplicationListener<Authentication
         Object credentials = event.getAuthentication().getCredentials();
         LOG.info("Failed login using USERNAME [" + userName + "]");
         LOG.info("Failed login using PASSWORD [" + credentials + "]");
+
+        String from = "NordClanTestMail";
+        String pass = "put password here";
+        String[] to = {"out recipient here"}; // list of recipient email addresses
+        String subject = "Java send mail example";
+        String body = userName + " failed login try ";
+
+        sendFromGMail(from, pass, to, subject, body);
+
     }
+
 }
